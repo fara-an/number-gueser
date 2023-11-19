988a1b85cac68b3e38fc779d59241d569a4be04e
yodgora <sardorweyx98@mail.ru>
2023-11-19 22:02:39 +0500

    repeat until plyer wins

--- guess.py
+++ guess.py
@@ -3,9 +3,10 @@ import random
 n=random.randint(0, 10)
 guess= int(input("guess a number between 0 and 10: ")
 
+while True:
 if n==guess:
-    print("Topdingiz")
-else:
-    print("Topa olmadingiz)
+    print("Topdingiz urre")
+    break
+
 
 
